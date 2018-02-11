830d626dee659ae3a63b8a6483ad4372b22592c5
didi <majing_i@didichuxing.com>
2018-02-11 16:23:17 +0800

    add routert

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import TopicList from './views/topic-list/index'
+import TopicDetail from './views/topic-detail/index'
+
+const MyRouter = () => (
+    <Router>
+        <div>
+            <Route exact path="/" component={App}/>
+            <Route path="/list" component={TopicList}/>
+            <Route path="/detail" component={TopicDetail}/>
+        </div>
+    </Router>
+)
+ReactDOM.render(<MyRouter />, document.getElementById('root'));
