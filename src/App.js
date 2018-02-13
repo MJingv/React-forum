5e9b9fb3101034066a945c341d77d2fef2e9153c
didi <majing_i@didichuxing.com>
2018-02-13 10:33:18 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends React.Component {
-    componentDidMount() {
-        // do something here
-    }
-
-    render() {
-        return (
-            <div>This is app</div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
+
+export default App;
--- src/views/topic-detail/index.jsx
+++ src/views/topic-detail/index.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import {observer,inject} from 'mobx-react';
 
-@inject('appState')
-@observer
 export default class TopicDetail extends React.Component {
     componentDidMount() {
         // do something here
-        console.log(this.props.appState)
     }
 
     render() {
         return (
-            <div>i am detail + {this.props.appState.msg} </div>
+            <div>This is topic list</div>
         )
     }
 }
